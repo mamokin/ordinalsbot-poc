636304b5b48bc927e17dc3a69b0cb18be4b250fe
underwood_inc <m.seaward@pm.me>
2024-08-09 14:43:59 -0300

    fix: add fix to account for inconsistent types from API

--- app/order-status/[id]/schema.ts
+++ app/order-status/[id]/schema.ts
@@ -1,59 +1,69 @@
 import { z } from 'zod';
 
-const orderStatusTxSchema = z.object({
-  commit: z.string(),
-  inscription: z.string(),
-  reveal: z.string(),
-  fees: z.string().optional(), // not marked optional in docs
-  total_fees: z.number().optional(), // not documented
-  updated_at: z.string().optional() // not documented
-});
+const orderStatusTxSchema = z
+  .object({
+    commit: z.string().optional(),
+    inscription: z.string().optional(),
+    reveal: z.string().optional(),
+    fees: z.string().optional(), // not marked optional in docs
+    total_fees: z.number().optional(), // not documented
+    updated_at: z.string().optional() // not documented
+  })
+  .optional();
 
-const orderStatusFileSchema = z.object({
-  completed: z.boolean(),
-  inscriptionId: z.string(),
-  iqueued: z.boolean(),
-  iqueuedAt: z.number(),
-  name: z.string(),
-  processing: z.boolean(),
-  s3Key: z.string(),
-  sent: z.string(),
-  size: z.number(),
-  status: z.string(),
-  tx: orderStatusTxSchema.optional(),
-  type: z.string(),
-  url: z.string(),
-  webhookUrl: z.string()
-});
+const orderStatusFileSchema = z
+  .object({
+    completed: z.boolean().optional(),
+    inscriptionId: z.string().optional(),
+    iqueued: z.boolean().optional(),
+    iqueuedAt: z.number().optional(),
+    name: z.string().optional(),
+    processing: z.boolean().optional(),
+    s3Key: z.string().optional(),
+    sent: z.string().optional(),
+    size: z.number().optional(),
+    status: z.string().optional(),
+    tx: orderStatusTxSchema.optional(),
+    type: z.string().optional(),
+    url: z.string().optional(),
+    webhookUrl: z.string().optional()
+  })
+  .optional();
 
-const orderStatusLightningInvoiceSchema = z.object({
-  expires_at: z.number(),
-  payreq: z.string()
-});
+const orderStatusLightningInvoiceSchema = z
+  .object({
+    expires_at: z.number().optional(),
+    payreq: z.string().optional()
+  })
+  .optional();
 
-const orderStatusChainInvoiceSchema = z.object({
-  address: z.string()
-});
+const orderStatusChainInvoiceSchema = z
+  .object({
+    address: z.string().optional()
+  })
+  .optional();
 
-const orderStatusChargeSchema = z.object({
-  address: z.string(),
-  amount: z.number(),
-  auto_settle: z.boolean(),
-  chain_invoice: orderStatusChainInvoiceSchema,
-  created_at: z.number(),
-  currency: z.string(),
-  desc_hash: z.boolean(),
-  description: z.string(),
-  fiat_value: z.number(),
-  hosted_checkout_url: z.string(),
-  id: z.string(),
-  lightning_invoice: orderStatusLightningInvoiceSchema,
-  source_fiat_value: z.number(),
-  status: z.string(),
-  ttl: z.number(),
-  uri: z.string(),
-  callback_url: z.string()
-});
+const orderStatusChargeSchema = z
+  .object({
+    address: z.string().optional(),
+    amount: z.number().optional(),
+    auto_settle: z.boolean().optional(),
+    chain_invoice: orderStatusChainInvoiceSchema,
+    created_at: z.number().optional(),
+    currency: z.string().optional(),
+    desc_hash: z.boolean().optional(),
+    description: z.string().optional(),
+    fiat_value: z.number().optional(),
+    hosted_checkout_url: z.string().optional(),
+    id: z.string().optional(),
+    lightning_invoice: orderStatusLightningInvoiceSchema,
+    source_fiat_value: z.number().optional(),
+    status: z.string().optional(),
+    ttl: z.number().optional(),
+    uri: z.string().optional(),
+    callback_url: z.string().optional()
+  })
+  .optional();
 /**
  * GET https://api.ordinalsbot.com/order
  *
@@ -63,32 +73,32 @@ const orderStatusChargeSchema = z.object({
 export const orderStatusSchema = z.object({
   reason: z.string().optional(), // internal server error schema
   error: z.string().optional(), // OK error schema
-  status: z.string(), // OK normal, OK error, & internal server error schemas
-  paid: z.boolean(),
+  status: z.string().optional(), // OK normal, OK error, & internal server error schemas
+  paid: z.boolean().optional(),
   underpaid: z.boolean().optional(),
   expired: z.boolean().optional(),
-  tx: orderStatusTxSchema.optional(),
+  tx: orderStatusTxSchema,
   sent: z.string().optional(),
-  baseFee: z.number(),
-  chainFee: z.number(),
+  baseFee: z.union([z.number().optional(), z.string().optional()]),
+  chainFee: z.number().optional(),
   charge: orderStatusChargeSchema.optional(),
-  completed: z.boolean(),
-  createdAt: z.number(),
-  fee: z.number(),
+  completed: z.boolean().optional(),
+  createdAt: z.number().optional(),
+  fee: z.union([z.number().optional(), z.string()]),
   files: z.array(orderStatusFileSchema).optional(),
-  id: z.string(),
-  inscribedCount: z.number(),
-  lowPostage: z.boolean(),
-  orderType: z.string(),
+  id: z.string().optional(),
+  inscribedCount: z.number().optional(),
+  lowPostage: z.boolean().optional(),
+  orderType: z.string().optional(),
   // paid: z.object({}),
-  paidAt: z.number(),
-  postage: z.number(),
-  processing: z.boolean(),
-  processingAt: z.number(),
-  rareSatsFee: z.number(),
-  receiveAddress: z.string(),
-  serviceFee: z.number(),
-  state: z.string()
+  paidAt: z.number().optional(),
+  postage: z.number().optional(),
+  processing: z.boolean().optional(),
+  processingAt: z.number().optional(),
+  rareSatsFee: z.number().optional(),
+  receiveAddress: z.string().optional(),
+  serviceFee: z.number().optional(),
+  state: z.string().optional()
 });
 
 export type OrderStatus = z.infer<typeof orderStatusSchema>;
